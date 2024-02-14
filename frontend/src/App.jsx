f2ceefe7123d618bffce3cd898871c8f497e4bf8
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2024-02-14 12:18:09 -0500

    Moved Frontend calls to Backend to HomePage.jsx and cleaned up App,jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,16 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage';
 
 function App() {
-  const [file, setFile] = useState(null);
-
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!file) return;
-
-    const formData = new FormData();
-    formData.append('excelFile', file);
-
-    try {
-      const response = await axios.post('http://localhost:5001/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      alert(`File uploaded. Content of A1: ${response.data}`);
-    } catch (error) {
-      console.error('Error uploading file:', error);
-      alert('Error uploading file to backend server.');
-    }
-  };
-
   return (
-    <div>
-      <h2>Upload Excel Sheet to display A1's contents</h2>
-      <form onSubmit={handleSubmit} encType="multipart/form-data">
-        <input type="file" name="excelFile" accept=".xlsx" required onChange={handleFileChange} />
-        <button type="submit">Upload</button>
-      </form>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
