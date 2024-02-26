f4917b37d663c1cc18e44a3c6a309864861454cc
MillerMaker <cohen11miller@gmail.com>
2024-02-26 11:10:25 -0500

    return Item Information

--- backend/src/controllers/vendibilityController.js
+++ backend/src/controllers/vendibilityController.js
@@ -1,13 +1,29 @@
-const ExcelJS = require('exceljs'); // Import exceljs
-const db = require('../config/DatabaseConnection');
+const {client} = require('../config/DatabaseConnection');
 
 exports.itemVendibility = async function (req, res) {
-    
+    //get parameters from request
     const { sessionId, itemId } = req.query;
 
-    console.log("Request data: " + req.query)
-    console.log(`Received a new request for item: ${itemId} and session: ${sessionId}`);
+    /* -------------------------------*/
+    /* VENDIBILITY ANALYSIS GOES HERE */
+    /* -------------------------------*/
 
-    // Send back a response
-    res.status(200).json({ message: `Received request for item: ${itemId} and session: ${sessionId}` });
+    try {
+        //Connect to database
+        await client.connect();
+
+        //Query Database for Item Info
+        const item = await client.db("Backend_Database").collection("Item").findOne({_id: Number(itemId)});
+
+        //log item to console
+        console.log(`Processed item vendibility request for item with id: ${item._id}`);
+
+        res.json(item); // Return the item object as JSON response
+
+    } catch (e) {
+        console.log("Error querying database");
+        res.status(500).json({error: "Error querying database" });
+    } finally {
+        await client.close();
+    }
 }
