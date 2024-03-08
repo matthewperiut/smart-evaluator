6077cd7be8c5b8de79164c029c7fa4724f040c7c
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2024-03-08 11:04:58 -0500

    Moved table display logic to TableDisplay component

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,53 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import ExcelJS from 'exceljs';
 import './HomePage.css';
 import Header from '../Header/Header';
+import TableDisplay from '../TableDisplay/TableDisplay';
 import VendibilityRequestDemo from './VendibilityRequestDemo'
 
 const HomePage = () => {
     const [file, setFile] = useState(null);
     const [fileName, setFileName] = useState('');
     const [excelData, setExcelData] = useState([]);
-    const [filteredData, setFilteredData] = useState([]);
-    const [columnHeaders, setColumnHeaders] = useState([]);
     const [solutionName, setSolutionName] = useState('');
     const [areaType, setAreaType] = useState('');
     const [isExcelUploaded, setIsExcelUploaded] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const [selectedRows, setSelectedRows] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [debouncedSearchQuery, setDebouncedSearchQuery] = useState('');
 
     useEffect(() => {
         const uploadedExcel = localStorage.getItem('uploadedExcel');
-        setFileName(localStorage.getItem("fileName"));
         if (uploadedExcel) {
-            const parsedExcelData = JSON.parse(uploadedExcel);
-            setExcelData(parsedExcelData);
-            setFilteredData(parsedExcelData);
-        }
-        if (excelData.length > 0) {
-            setColumnHeaders(excelData[0]);
+            setExcelData(JSON.parse(uploadedExcel));
+            setIsExcelUploaded(true);
+            setFileName(localStorage.getItem("fileName"));
+            
         }
     }, []);
-
-    useEffect(() => {
-        if (debouncedSearchQuery === '') {
-            setFilteredData(excelData);
-        } else {
-            filterData(debouncedSearchQuery);
-        }
-    }, [debouncedSearchQuery, excelData]);
-
-    useEffect(() => {
-        const delayDebounceFn = setTimeout(() => {
-            setDebouncedSearchQuery(searchQuery);
-        }, 10000);
-
-        return () => clearTimeout(delayDebounceFn);
-    }, [searchQuery]);
 
     const readExcel = async (file) => {
         try {
@@ -80,7 +56,6 @@ const HomePage = () => {
             });
             setExcelData(rows);
             setFileName(file.name);
-            setFilteredData(rows);
             localStorage.setItem('uploadedExcel', JSON.stringify(rows));
             localStorage.setItem('fileName', file.name);
         } catch (error) {
@@ -107,8 +82,6 @@ const HomePage = () => {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-            //Print to DOM
-            seta1(`Session ID: ${response.data._id} \n ItemIDs: ${response.data.uncompleted_items}`);
 
             //Print to console
             console.log(`New Session Created! \n 
@@ -124,7 +97,9 @@ const HomePage = () => {
 
     const toggleModal = async (e) => {
         var modal = document.getElementById("generateSolution");
-        modal.style.display = modal.style.display === "block" ? "none" : "block";
+        if (modal) {
+            modal.style.display = modal.style.display === "block" ? "none" : "block";
+        }
     }
 
     const handleFileInputChange = async (e) => {
@@ -133,56 +108,17 @@ const HomePage = () => {
         console.log(file);
     }
 
-    const toggleRow = (rowIndex) => {
-        if (selectedRows.includes(rowIndex)) {
-            setSelectedRows(selectedRows.filter(row => row !== rowIndex));
-        }
-        else{
-            setSelectedRows([...selectedRows, rowIndex]);
-        }
-    }
-
-
-    // Function to filter data based on search query
-    const filterData = (query) => {
-        const filtered = excelData.filter((row, index) => {
-            // Always include the first row (headers)
-            if (index === 0) return true;
-            // Filter rows based on search query
-            return row.some((cell) => String(cell).toLowerCase().includes(query));
-        });
-        // Set filtered data including the first row (headers)
-        setFilteredData(filtered.length > 0 ? filtered : excelData);
-    };
-    
-    // Function to handle search input change
-    const handleSearchInputChange = (e) => {
-        const query = e.target.value.toLowerCase();
-        setSearchQuery(query);
-        filterData(query);
-    };
-
     return (
         <div>
             <Header />
             <div>
                 <div>
-                {!excelData.length > 0 ? (
+                {!isExcelUploaded ? (
                     <h2 className = 'font-poppins text-green absolute left-4 top-20 md:absolute p-4 rounded-lg text-sm md:text-lg'>Work Dashboard</h2>
                 ) : (
                     <h2 className='font-poppins text-green absolute left-4 top-20 md:absolute p-4 rounded-lg text-sm md:text-lg'>{fileName}</h2>
                 )}
                 <button className=' hidden lg:block bg-green text-black absolute w-60 h-12 right-4 top-20 m-4' onClick={toggleModal}>+ Generate New Solution</button>
-                <div className ="fixed left-4 top-36 p-4">
-                <input type="text" placeholder="Search..." className="px-4 text-xs border border-gray-400 rounded-md bg-gray-200 w-80 text-left" placeholder-class="text-gray-400 font-bold text-xl"  value={searchQuery} onChange={handleSearchInputChange}/>
-                <button className = "hidden lg:block fixed top-40 right-44">Export All</button>
-                <button className = "hidden lg:block fixed top-40 right-6">Export Selected</button>
-                {/* <div className="magnify"></div> */}
-                <br />
-                <br />
-                </div>
-                
-                
                 
                 {/* Modal */}
                 <div id="generateSolution" className='modal-filter'>
@@ -310,42 +246,7 @@ const HomePage = () => {
                         <div className = "animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-green">
                         </div>
                     )}
-                <div className='display-box'>
-                    {filteredData.length > 0 && (
-                        <div className="scrollable-container">
-                            <table>
-                                <tbody>
-                                    {filteredData.map((row, rowIndex) => (
-                                        <tr key={rowIndex} className={selectedRows.includes(rowIndex) ? 'selected-row' : ''}>
-                                            <td>
-                                                <input
-                                                    type="checkbox"
-                                                    checked={selectedRows.includes(rowIndex)}
-                                                    onChange={() => toggleRow(rowIndex)}
-                                                    disabled={rowIndex== 0}
-                                                    />
-                                            </td>
-                                            {row.map((cell, cellIndex) => (
-                                                <td key={cellIndex}>
-                                                    {rowIndex >= 1 && cellIndex === 1 ? (
-                                                        <Link
-                                                            to={"/itemDetails"}
-                                                            state={{ rowData: row, columnHeaders: columnHeaders }}
-                                                        >
-                                                            {cell}
-                                                        </Link>
-                                                    ) : (
-                                                        cell
-                                                    )}
-                                                </td>
-                                            ))}
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                    )}
-                </div>
+                <TableDisplay excelData={excelData}/>
             </div>
         </div>
         </div>
---
+++ frontend/src/components/TableDisplay/TableDisplay.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './TableDisplay.css';
+
+
+
+const TableDisplay = ({excelData}) => {
+    const [filteredData, setFilteredData] = useState([]);
+    const [selectedRows, setSelectedRows] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [debouncedSearchQuery, setDebouncedSearchQuery] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const uploadedExcel = localStorage.getItem('uploadedExcel');
+        if (uploadedExcel) {
+            const parsedExcelData = JSON.parse(uploadedExcel);
+            setFilteredData(parsedExcelData)
+        } else if (excelData) {
+            setFilteredData(excelData);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (debouncedSearchQuery === '') {
+            setFilteredData(excelData);
+        } else {
+            filterData(debouncedSearchQuery);
+            
+        }
+    }, [debouncedSearchQuery, excelData]);
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            setDebouncedSearchQuery(searchQuery);
+        }, 10000);
+
+        return () => clearTimeout(delayDebounceFn);
+    }, [searchQuery]);
+
+    const toggleRow = (rowIndex) => {
+        if (selectedRows.includes(rowIndex)) {
+            setSelectedRows(selectedRows.filter(row => row !== rowIndex));
+        }
+        else{
+            setSelectedRows([...selectedRows, rowIndex]);
+        }
+    }
+
+    // Function to filter data based on search query
+    const filterData = (query) => {
+        const filtered = excelData.filter((row, index) => {
+            // Always include the first row (headers)
+            if (index === 0) return true;
+            // Filter rows based on search query
+            return row.some((cell) => String(cell).toLowerCase().includes(query));
+        });
+        // Set filtered data including the first row (headers)
+        setFilteredData(filtered.length > 0 ? filtered : excelData);
+    };
+    
+    // Function to handle search input change
+    const handleSearchInputChange = (e) => {
+        const query = e.target.value.toLowerCase();
+        setSearchQuery(query);
+        filterData(query);
+    };
+
+    return (
+        <div>
+            <div className="fixed left-4 top-36 p-4">
+                <input type="text" placeholder="Search..." className="px-4 text-xs border border-gray-400 rounded-md bg-gray-200 w-80 text-left" placeholder-class="text-gray-400 font-bold text-xl" value={searchQuery} onChange={handleSearchInputChange} />
+                <button className="hidden lg:block fixed top-40 right-44">Export All</button>
+                <button className="hidden lg:block fixed top-40 right-6">Export Selected</button>
+                <br />
+                <br />
+            </div>
+            {isLoading && (
+                        <div className = "animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-green">
+                        </div>
+                    )}
+            <div className='display-box'>
+                {filteredData.length > 0 && (
+                    <div className="scrollable-container">
+                        <table>
+                            <tbody>
+                                {filteredData.map((row, rowIndex) => (
+                                    <tr key={rowIndex} className={selectedRows.includes(rowIndex) ? 'selected-row' : ''}>
+                                        <td>
+                                            <input
+                                                type="checkbox"
+                                                checked={selectedRows.includes(rowIndex)}
+                                                onChange={() => toggleRow(rowIndex)}
+                                                disabled={rowIndex == 0}
+                                            />
+                                        </td>
+                                        {row.map((cell, cellIndex) => (
+                                            <td key={cellIndex}>
+                                                {rowIndex >= 1 && cellIndex === 1 ? (
+                                                    <Link
+                                                        to={"/itemDetails"}
+                                                        state={{ rowData: row, columnHeaders: excelData[0] }}
+                                                    >
+                                                        {cell}
+                                                    </Link>
+                                                ) : (
+                                                    cell
+                                                )}
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default TableDisplay;
